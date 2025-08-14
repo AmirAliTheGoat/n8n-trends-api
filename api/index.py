8f4bfb0babbdc2ca90327df83095c7e0dec8d16b
AmirAliTheGoat <141083554+AmirAliTheGoat@users.noreply.github.com>
2025-08-15 02:19:58 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"status": "ok", "message": "Hello from Vercel Python FastAPI"}
+
 @app.get("/get_trends")
-def read_root():
-    return {"message": "Hello World"}
+def get_trends():
+    return JSONResponse({"ok": True, "data": []})
